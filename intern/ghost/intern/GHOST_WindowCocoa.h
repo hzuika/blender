68791b287a896314ed6252b7cc5bb3ca3e0adcc7
hzuika <39191601+hzuika@users.noreply.github.com>
2021-06-24 21:11:17 +0900

    WIP input method: add comment

--- intern/ghost/intern/GHOST_WindowCocoa.h
+++ intern/ghost/intern/GHOST_WindowCocoa.h
@@ -358,7 +358,7 @@ enum {
   IME_ENABLED = (1 << 1),
   IME_COMPOSING = (1 << 2),
   KEY_CONTROLCHAR = (1 << 3),
-  IME_COMPOSITION_EVENT = (1 << 4),
-  IME_RESULT_EVENT = (1 << 5)
+  IME_COMPOSITION_EVENT = (1 << 4), // For Korean input
+  IME_RESULT_EVENT = (1 << 5) // For Korean input
 };
 #endif
